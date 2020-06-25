6caadbecbf7da6553eb4d9a5453fcb433b9f0458
leeys <leeys9423@gmail.com>
2020-06-25 10:11:00 +0900

    20200624

---
+++ 20200624.sql
@@ -0,0 +1,21 @@
+SELECT *
+FROM prod;
+
+SELECT prod_id, prod_name
+FROM prod;
+
+1번문제
+SELECT *
+FROM lprod;
+
+2번문제
+SELECT buyer_id, buyer_name
+FROM buyer;
+
+3번문제
+SELECT *
+FROM cart;
+
+4번문제
+SELECT mem_id, mem_pass, mem_name
+FROM member;
